98e918466f6c7e9b6e56c613ff91a6405d243fd7
hermeson883 <hermesondovalebeserra722@gmail.com>
2024-02-17 22:37:18 -0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Customer(){
+    return(
+        <h1>Hello Customers</h1>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Invoice(){
+    return(
+        <h1>Invoice Page</h1>
+    )
+}
