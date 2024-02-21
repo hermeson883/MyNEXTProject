e6ce981c9c059ad7efd0efd3e767cf4d18f99981
hermeson883 <hermesondovalebeserra722@gmail.com>
2024-02-21 17:40:13 -0300

    adding 404 router

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
-'use server'
+'use server' // Usando o serivdor e tudo que está no arquivo é uma server function
 import { z } from 'zod'
-
+import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 const formSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -9,19 +11,63 @@ const formSchema = z.object({
   date: z.string(),
 })
 
-interface a {z.object({id:z.string()})}.
-const createSchema = formSchema.omit({id:true, date:true})
+const CreateInvoice = formSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  }) // Validando o schema com parse
   try {
-    const rawFormData = {
-      custormersId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    }
-    console.log(rawFormData)
-    console.log(typeof rawFormData.amount)
+    const amountInCents = amount * 100 // Transformando em centavos
+    const date = new Date().toISOString().split('T')[0]
+
+    await sql`
+            INSERT INTO 
+                invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `
+    revalidatePath('/dashboard/invoices') // funciona como o reset do 'hookForms', porém limpa a rota do cache do next e depois faz uma nova soliçitação ao servidor
+    console.log('Adicionado')
   } catch (e) {
     console.log(e)
+  }
+  redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = formSchema.omit({ id: true, date: true })
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+  const amountInCents = amount * 100
+  try {
+    await sql`
+      UPDATE invoices SET 
+        customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `
+  } catch (e) {
+    console.log(e)
+  }
+  revalidatePath('/dashboard/invoices') // Fazendo uma nova requisição para o servidor
+  redirect('/dashboard/invoices') // Fazendo um redirecionamento
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice')
+
+  try {
+    await sql`
+      DELETE FROM invoices WHERE id = ${id}
+      `
+    revalidatePath('/dashboard/invoices')
+    return { message: 'Deleted Invoice' }
+  } catch (e) {
+    return { message: 'Failed to DELETE invoice' }
   }
 }
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
-const styleguide = require('@vercel/style-guide/prettier');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const styleguide = require('@vercel/style-guide/prettier')
 
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
-};
+}
